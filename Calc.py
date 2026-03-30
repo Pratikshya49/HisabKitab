ba2e634addf7f04979fbc652c250eed7b7bce568
Lazana Shrestha <np03cs4a240021@heraldcollege.edu.np>
2026-03-30 15:05:47 +0545

    Add multiplication function by Lazana

---
+++ Calc.py
@@ -0,0 +1,8 @@
+
+def multiply(num1, num2):
+    return num1 * num2
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = multiply(num1, num2)
+print(f"{num1} × {num2} = {result}")
